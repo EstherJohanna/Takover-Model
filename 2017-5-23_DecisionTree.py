3d7b57cc7f53db826d587301e92e200954495c69
EstherJohanna <estjohbos@gmail.com>
2017-05-31 17:22:59 +0200

    ortools works, nicer code, dangerousness implemented

--- 2017-5-23_DecisionTree.py
+++ 2017-5-23_DecisionTree.py
@@ -2,69 +2,58 @@ from pyschedule import solvers, plotters, Scenario
 import Sclass
 
 #input from GUI
-expert = 0
-gazeoff = 0 # always means headoff, too!
+expert = 1
+gazeoff = 1 # always means headoff, too!
 longgaze = 1 
-bodyTurned = 0
-Lockout = 1
-handsOccupied = 0
-distanceHands = 0
-feetOnPedals = 1
-dangerousScenario = 0
-drivingSpeed = 100 #in m
+bodyTurned = 1
+Lockout = 0
+handsOccupied = 0#hier gibt es ein problem!
+distanceHands = 50
+feetOnPedals = 0
+dangerousScenario = 1
+drivingSpeed = 100 #in km/h
 perceivedUrgency = 0
-S = Scenario('takeover', horizon=4000)
+S = Scenario('takeover', horizon=10000)
 
 
 def main():
     #create instance of a situation
-    currSituation = Sclass.situation(dangerousScenario, S)
+    currSituation = Sclass.situation(dangerousScenario, S, distanceHands, drivingSpeed)
     Sclass.situation.resources(currSituation)
-    #decision tree
-    if not dangerousScenario:
-        if expert:
-            Sclass.situation.exp(currSituation)
-        else:         
+    
+    #decision tree       
+    if not expert:
+        if not dangerousScenario:
             Sclass.situation.nov(currSituation)
-        Sclass.situation.start(currSituation)
-        if not Lockout:
-            Sclass.situation.lockout(currSituation)
-        if gazeoff:  
-            Sclass.situation.gazeOnRoad(currSituation)
-            if longgaze:
-                Sclass.situation.turnSA(currSituation) 
-            if bodyTurned:
-                Sclass.situation.turnBody(currSituation)
-        if not gazeoff:
-            if not longgaze:#if the driver JUST started looking to the street (he does not need
-    #to turn the head anymore, but needs to build up situation awareness)
-                Sclass.situation.SA(currSituation)
-        if handsOccupied:
-            Sclass.situation.hands2wheel(currSituation)
-            Sclass.situation.unoccupyHands(currSituation)
-        if not handsOccupied:
-            if distanceHands > 0:
-                Sclass.situation.hands2wheel(currSituation)
-        if not feetOnPedals:
-            Sclass.situation.feet2pedal(currSituation) 
-        Sclass.situation.TO(currSituation)
-        
-    if dangerousScenario:#I expect the tone to be different = driver knows immediately it's dangerous
+    else:
         Sclass.situation.exp(currSituation)
-        Sclass.situation.start(currSituation)
-        if gazeoff:  
-            Sclass.situation.gazeOnRoad(currSituation)
+        
+    Sclass.situation.start(currSituation)####
+    if not Lockout:
+        if not dangerousScenario:
+            Sclass.situation.lockout(currSituation)
+    if gazeoff:  
+        Sclass.situation.gazeOnRoad(currSituation)
+        if longgaze:
+            Sclass.situation.turnSA(currSituation) 
         if bodyTurned:
-                Sclass.situation.turnBody(currSituation)
-        if handsOccupied:
+            Sclass.situation.turnBody(currSituation)
+    if not gazeoff:
+        if not longgaze:#if the driver JUST started looking to the street (he does not need
+#to turn the head anymore, but needs to build up situation awareness)
+            Sclass.situation.SA(currSituation)
+    if handsOccupied:
+        Sclass.situation.hands2wheel(currSituation)
+        Sclass.situation.unoccupyHands(currSituation)
+    if not handsOccupied:
+        if distanceHands > 0:
             Sclass.situation.hands2wheel(currSituation)
-            Sclass.situation.unoccupyHands(currSituation)#change target size!
-        if not handsOccupied:
-            if distanceHands > 0:
-                Sclass.situation.hands2wheel(currSituation)
-        if not feetOnPedals:
-            Sclass.situation.feet2pedal(currSituation) 
-        Sclass.situation.TO(currSituation)
+    if not feetOnPedals:
+        Sclass.situation.feet2pedal(currSituation) 
+    Sclass.situation.TO(currSituation)
+        
+#I expect the tone to be different = driver knows immediately it's dangerous
+        
     return
 
 if __name__ == '__main__':
@@ -78,7 +67,7 @@ def run(S) :
     #ortools or mip before solve
     if solvers.ortools.solve(S):
         
-        plotters.matplotlib.plot(S,fig_size=(40,5))
+        plotters.matplotlib.plot(S,fig_size=(100,5))
     else:
         print('no solution exists')
 run(S)
