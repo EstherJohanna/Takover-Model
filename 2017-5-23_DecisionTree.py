fc795bd46273ae36cc64d259567ecfe786a59c17
EstherJohanna <estjohbos@gmail.com>
2017-06-07 09:28:12 +0200

    with loop

--- 2017-5-23_DecisionTree.py
+++ 2017-5-23_DecisionTree.py
@@ -1,69 +1,68 @@
-from pyschedule import solvers, plotters, Scenario    
+from pyschedule import solvers, plotters
 import Sclass
 
-#ToDo: input from GUI
-expert = 1
-gazeoff = 1 # always means headoff, too!
-longgaze = 1 
-bodyTurned = 1
-Lockout = 0
-handsOccupied = 1
-distanceHands = 50
-feetOnPedals = 0
-dangerousScenario = 0
-drivingSpeed = 100 #in km/h
-perceivedUrgent = 1
-S = Scenario('takeover', horizon=20000)
-
-def main():
-    #create instance of a situation
-    currSituation = Sclass.situation(dangerousScenario, S, distanceHands, drivingSpeed, perceivedUrgent)
-    Sclass.situation.resources(currSituation)
+class makeSchedule(object):
     
-    #decision tree       
-    if not expert:
-        if not dangerousScenario:
-            Sclass.situation.nov(currSituation)
-    else:
-        Sclass.situation.exp(currSituation)
+    def inputs(self, expert, gazeoff, longgaze, bodyTurned, lockout, handsOccupied, distanceHands, feetOnPedals, dangerousScenario, drivingSpeed, perceivedUrgent, S):
+    #ToDo: input from GUI
+        self.expert = expert
+        self.gazeoff = gazeoff # always means headoff, too!
+        self.longgaze = longgaze 
+        self.bodyTurned = bodyTurned
+        self.lockout = lockout
+        self.handsOccupied = handsOccupied
+        self.distanceHands = distanceHands
+        self.feetOnPedals = feetOnPedals
+        self.dangerousScenario = dangerousScenario
+        self.drivingSpeed = drivingSpeed #in km/h
+        self.perceivedUrgent = perceivedUrgent
+        self.S = S
+    
+    def main(self):
+        #create instance of a situation
+        currSituation = Sclass.situation(self.dangerousScenario, self.S, self.distanceHands, self.drivingSpeed, self.perceivedUrgent)
+        Sclass.situation.resources(currSituation)
         
-    Sclass.situation.start(currSituation)
-    if not Lockout:
-        if not dangerousScenario:
-            Sclass.situation.lockout(currSituation)
-    if gazeoff:  
-        Sclass.situation.gazeOnRoad(currSituation)
-        if longgaze:
-            Sclass.situation.turnSA(currSituation) 
-        if bodyTurned:
-            Sclass.situation.turnBody(currSituation)
-    if not gazeoff:
-        if not longgaze:#if the driver JUST started looking to the street (he does not need
-#to turn the head anymore, but needs to build up situation awareness)
-            Sclass.situation.SA(currSituation)
-    if handsOccupied:
-        Sclass.situation.hands2wheel(currSituation)
-        Sclass.situation.unoccupyHands(currSituation)
-    if not handsOccupied:
-        if distanceHands > 0:
+        #decision tree       
+        if not self.expert:
+            if not self.dangerousScenario:
+                Sclass.situation.nov(currSituation)
+            else:
+                Sclass.situation.exp(currSituation)
+        else:
+            Sclass.situation.exp(currSituation)
+            
+        Sclass.situation.start(currSituation)
+        if not self.lockout:
+            if not self.dangerousScenario:
+                Sclass.situation.lockout(currSituation)
+        if self.gazeoff:  
+            Sclass.situation.gazeOnRoad(currSituation)
+            if self.longgaze:
+                Sclass.situation.turnSA(currSituation) 
+            if self.bodyTurned:
+                Sclass.situation.turnBody(currSituation)
+        if not self.gazeoff:
+            if not self.longgaze:#if the driver JUST started looking to the street (he does not need
+    #to turn the head anymore, but needs to build up situation awareness)
+                Sclass.situation.SA(currSituation)
+        if self.handsOccupied:
             Sclass.situation.hands2wheel(currSituation)
-    if not feetOnPedals:
-        Sclass.situation.feet2pedal(currSituation) 
-    Sclass.situation.TO(currSituation)        
-    return
+            Sclass.situation.unoccupyHands(currSituation)
+        if not self.handsOccupied:
+            if self.distanceHands > 0:
+                Sclass.situation.hands2wheel(currSituation)
+        if not self.feetOnPedals:
+            Sclass.situation.feet2pedal(currSituation) 
+        Sclass.situation.TO(currSituation)        
+        return
 
-if __name__ == '__main__':
-    main()
-    
-#S.use_makespan_objective()
-#print(S)
-###############################################################################
-# A small helper method to solve and plot a scenario
-def run(S) :
-    #ortools or mip before solve
-    if solvers.ortools.solve(S):
-        
-        plotters.matplotlib.plot(S,fig_size=(150,5))
-    else:
-        print('no solution exists')
-run(S)
+    ###############################################################################
+    # A small helper method to solve and plot a scenario
+    def run(self) :
+        #ortools or mip before solve
+        if solvers.ortools.solve(self.S):
+            
+            plotters.matplotlib.plot(self.S,fig_size=(150,5))
+        else:
+            print('no solution exists')
