54aba42aefe28ae6b11815029d7d7f89d7a7a696
EstherJohanna <estjohbos@gmail.com>
2017-06-07 12:31:54 +0200

    loop

--- runManyTimes.py
+++ runManyTimes.py
@@ -1,22 +1,31 @@
 
 import DecisionTree
 from pyschedule import Scenario
+import numpy
+import matplotlib.pyplot as plt
 
-
+results = []
     
     
 def main():
     #create instance of a situation
     thisOne = DecisionTree.makeSchedule()
     
-    DecisionTree.makeSchedule.inputs(thisOne, 0,1,1,1,1,1,50,1,1,100,0,Scenario('takeover', horizon=20000))#(self, expert, gazeoff, longgaze, bodyTurned, lockout, handsOccupied, distanceHands, feetOnPedals, dangerousScenario, drivingSpeed, perceivedUrgent, S)
+    #inputs:(self, expert, gazeoff, longgaze, bodyTurned, lockout, handsOccupied, distanceHands, feetOnPedals, 
+    #dangerousScenario, drivingSpeed, perceivedUrgent, S)
+    DecisionTree.makeSchedule.inputs(thisOne, 0, 0, 1, 0, 1, 0, 0, 1, 1, 120, 1, Scenario('takeover', horizon=20000))
     DecisionTree.makeSchedule.main(thisOne)
-    DecisionTree.makeSchedule.run(thisOne)
+    results.append(DecisionTree.makeSchedule.run(thisOne))
+    
 
 
 
 if __name__ == '__main__':
     i = 0
-    for i in range (0,3):
+    for i in range (0,1000):
         main()
         i += 1
+    #print results
+    print numpy.mean(results)
+    print numpy.std(results)
+    plt.hist(results, bins=50)
