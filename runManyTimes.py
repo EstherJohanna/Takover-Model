f983db6e61f648aded1c0618b09b23470e5b2052
EstherJohanna <estjohbos@gmail.com>
2017-07-05 07:28:43 +0200

    Update runManyTimes.py

--- runManyTimes.py
+++ runManyTimes.py
@@ -3,24 +3,25 @@ import DecisionTree
 from pyschedule import Scenario, plotters
 import numpy
 import matplotlib.pyplot as plt
+from scipy.stats import gamma
 
 results = []
 
 #inputs
 a = []
 a.append(1) #expert
-a.append(0) #gaze off
+a.append(1) #gaze off
 a.append(1) #long gaze
-a.append(0) #body turned
+a.append(1) #body turned
 a.append(0) #lockout
-a.append(0) #handsOccupied
-a.append(0) #distanceHands
-a.append(1) #feet on pedals
-a.append(1) # dangerous scenario
-a.append(130) # driving speed
-a.append(1) # perceived urgent
-a.append(1) #from 1-3: 1 no task, 2 low mental workload, 3 high mental workload
-a.append(1) #traffic: 1 no traffic, 2 medium traffic, 3 much traffic    
+a.append(1) #handsOccupied
+a.append(60) #distanceHands
+a.append(0) #feet on pedals
+a.append(0) # dangerous scenario
+a.append(120) # driving speed
+a.append(0) # perceived urgency/ low time budget
+a.append(2) #from 1-3: 1 no task, 2 low mental workload, 3 high mental workload
+a.append(2) #traffic: 1 no traffic, 2 medium traffic, 3 much traffic    
     
 def main():
     #create instance of a situation
@@ -36,11 +37,24 @@ def main():
 
 
 if __name__ == '__main__':
-    i = 0
-    for i in range (0,100):
-        main()
-        i += 1
-    #print results
-    print numpy.mean(results)
-    print numpy.std(results)
-    plt.hist(results, bins=50)
+    
+    main()
+    
+    fig, ax = plt.subplots(1, 1)
+    r = gamma.rvs(a = 3, loc = results[0] - 3, size=1000)
+    ax.hist(r, bins = 50, normed=True, histtype='stepfilled', alpha=0.2)
+    ax.legend(loc='best', frameon=False)
+    plt.show()
+
+print 'mean is', numpy.mean(r)
+#==============================================================================
+#     
+#     i = 0
+#     for i in range (0,100):
+#         main()
+#         i += 1
+#     #print results
+#     print numpy.mean(results)
+#     print numpy.std(results)
+#     plt.hist(results, bins=50)
+#==============================================================================
