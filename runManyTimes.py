8e9a7a166ca0536c12707c3f4d31ea652ab0ff92
EstherJohanna <estjohbos@gmail.com>
2017-07-18 14:46:20 +0200

    Update runManyTimes.py

--- runManyTimes.py
+++ runManyTimes.py
@@ -1,9 +1,14 @@
 
+
+
+
 import DecisionTree
 from pyschedule import Scenario, plotters
 import numpy
-#import matplotlib.pyplot as plt
-#from scipy.stats import gamma
+import matplotlib.pyplot as plt
+from scipy.stats import gamma
+
+numpy.set_printoptions(threshold=numpy.nan)
 
 results = []
 
@@ -13,15 +18,15 @@ a.append(1) #expert
 a.append(0) #gaze off
 a.append(1) #long gaze
 a.append(0) #body turned
-a.append(1) #lockout
+a.append(1) #task is not automatically ended
 a.append(0) #handsOccupied
 a.append(30) #distanceHands
-a.append(1) #feet on pedals
+a.append(0) #feet on pedals
 a.append(0) # dangerous scenario
 a.append(120) # driving speed
-a.append(1) # perceived urgency/ low time budget
-a.append(1) #from 1-3: 1 no task, 2 low mental workload, 3 high mental workload
-a.append(1) #traffic: 1 no traffic, 2 medium traffic, 3 much traffic    
+a.append(0) # perceived urgency/ low time budget
+a.append(0) #0 no mental wokrload 1 mental workload
+a.append(3) #traffic: 1 no traffic, 2 medium traffic, 3 much traffic    
     
 def main():
     #create instance of a situation
@@ -37,19 +42,19 @@ def main():
 
 
 if __name__ == '__main__':
+    
+    main()
+    s = results[0] / 1000.0 #from milliseconds to seconds
+    fig, ax = plt.subplots(1, 1)
+    r = gamma.rvs(a = 3, loc = s - 3, size=17)
+    print r
+    ax.hist(r, bins = 50, normed=True, histtype='stepfilled', alpha=0.2)
+    ax.legend(loc='best', frameon=False)
+    plt.show()
 
- 
-#    main()
-#    
-#    fig, ax = plt.subplots(1, 1)
-#    r = gamma.rvs(a = 3, loc = results[0] - 3, size=1000)
-#    ax.hist(r, bins = 50, normed=True, histtype='stepfilled', alpha=0.2)
-#    ax.legend(loc='best', frameon=False)
-#    plt.show()
+print 'mean is', numpy.mean(r)
 
-#print 'mean is', numpy.mean(r)
-
-
+'''  
      i = 0
      for i in range (0,1):
          main()
@@ -59,3 +64,4 @@ if __name__ == '__main__':
      print numpy.std(results)
      #plt.hist(results, bins=50)
 
+'''
